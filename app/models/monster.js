cf94724ec9e5ec7e676ffc435e453bf6af169f19
ClumsyCoder <robert.burnfield@gmail.com>
2015-08-31 16:57:13 -0400

    Expanding schema to include additional information about the pokemon data.

--- app/models/monster.js
+++ app/models/monster.js
@@ -2,14 +2,29 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var monsterSchema = new Schema({
+    generation: Number,
     name: String,
-    dex_entry: Number
+    dex_entry: Number,
+    type: [String],
+    capture_rate: Number,
+    base_egg_steps: Number,
+    base_happiness: Number,
+    experience_growth: Number,
+    egg_groups: [String],
+    base_stats: {
+        hp: Number,
+        attack: Number,
+        defense: Number,
+        special_attack: Number,
+        special_defense: Number,
+        speed: Number
+    }
 });
 
 if (!monsterSchema.options.toJSON) {
     monsterSchema.options.toJSON = {};
 }
-monsterSchema.options.toJSON.transform = function (doc, ret, options) {
+monsterSchema.options.toJSON.transform = function (doc, ret) {
     // remove the _id of every document before returning the result
     delete ret._id;
 };
