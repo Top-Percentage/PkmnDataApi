f78552c6803b282fe716557c555132d12eb7e4a8
ClumsyCoder <robert.burnfield@gmail.com>
2015-08-31 16:39:27 -0400

    Splitting the get based on the parameter.  Need to specify if you are searching based on name or by dex id.

--- app/routes/monster.js
+++ app/routes/monster.js
@@ -4,7 +4,6 @@ var router = express.Router();
 
 var Monster = require('../models/monster');
 
-
 // define route for all monsters
 router.get('/', function (req, res) {
     Monster.find(function (err, monsters) {
@@ -16,27 +15,34 @@ router.get('/', function (req, res) {
     });
 });
 
-// define route for monsters
-router.get('/:pokemonId', function (req, res) {
-    var pokemonId = req.params.pokemonId || null;
-    console.log("Pokemon ID: " + pokemonId);
+// define route for fetching monster by dex number
+router.get('/dex/:dexId', function (req, res) {
+    var dexId = req.params.dexId || null;
+    if (!_.isUndefined(dexId) && !_.isNull(dexId) && !_.isNaN(dexId)) {
+        Monster.findByDexEntry(dexId, function (err, monster) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(monster);
+            }
+        });
+    } else {
+        // Invalid Params
+        res.json({'message': 'It is all wrong'});
+    }
+});
 
-    var handleRequest = function (err, monster) {
-        if (err) {
-            res.send(err);
-        } else {
-            res.json(monster);
-        }
-    };
-
-    if (!_.isUndefined(pokemonId) && !_.isNull(pokemonId)) {
-        if (_.isNaN(parseInt(pokemonId))) {
-            console.log("Search By Name");
-            Monster.findByName(pokemonId, handleRequest);
-        } else {
-            console.log("Search by Dex Entry");
-            Monster.findByDexEntry(pokemonId, handleRequest);
-        }
+// define route for monster by its name
+router.get('/name/:name', function (req, res) {
+    var name = req.params.name || null;
+    if (!_.isUndefined(name) && !_.isNull(name)) {
+        Monster.findByName(name, function (err, monster) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(monster);
+            }
+        });
     } else {
         // Invalid Params
         res.json({'message': 'It is all wrong'});
